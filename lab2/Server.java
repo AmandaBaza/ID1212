0148ac973e6fbfc887ccf6922e5030416f16b4f0
abaz <abaz@kth.se>
2021-11-11 22:38:25 +0100

    Update Server.java
    base

--- lab2/Server.java
+++ lab2/Server.java
@@ -1,3 +1,20 @@
+import java.net.*;
+import java.io.*;
+
 public class Server{
+
+    public static void main(String []args) throws IOException{
+        int port = 8080;
+        ServerSocket server = new ServerSocket(port);
+
+        while(true){
+            Socket client = server.accept();
+            OutputStream clientOutput =client.getOutputStream();
+            clientOutput.write("HTTP/1.1 200\r\n".getBytes());
+            clientOutput.write("\r\n".getBytes());
+            clientOutput.write("<b>Guessing Game<b>".getBytes());
+            clientOutput.flush();
+        }
+    }
 
 }
